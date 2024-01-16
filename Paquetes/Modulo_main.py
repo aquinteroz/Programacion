9fcad910fb12bac3017fcf091a69ed6f42119d40
Andrés Quintero Zea, PhD <andres.quintero27@eia.edu.co>
2024-01-16 15:06:55 -0500

    Update 2024-1

--- Paquetes/Modulo_main.py
+++
@@ -1,5 +0,0 @@
-def ident():
-    print('Función ident en Modulo_main.py')
-
-if __name__ == '__main__':
-    print('Esta sentencia solo se ejecuta si actúa Modulo_main.py como programa principal.')
